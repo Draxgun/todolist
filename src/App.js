da2e4d3a953f3fa481e09bcb8cadbfef0b658b0e
Jesus Alejandro Bañuelos Mendoza <76793906+Draxgun@users.noreply.github.com>
2023-11-27 16:39:27 -0600

    ToDoList excercise to practice react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Task } from "./Task"
 
 function App() {
+
+  let [todoList, setTodoList] = useState([]);
+  let [newTask,setnewTask] = useState("");
+
+  const handleChange = (event) => {
+    setnewTask(newTask = event.target.value);
+  }
+
+  const addTask  = () => {
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id + 1,
+      taskName: newTask,
+      completed: false,
+    }
+    
+    const newTodoList = [...todoList, task];
+    console.log(newTodoList)
+    setTodoList(todoList = newTodoList);
+  }
+
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => task.id !== id)
+    setTodoList(todoList = newTodoList);
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='addTask'>
+      <input onChange={handleChange}></input>
+      <button onClick={addTask}>Add Task</button>
     </div>
-  );
+
+    <div className='list'>
+    {todoList.map((task) => {
+        return <Task taskName = {task.taskName}  id = {task.id} deleteTask = {deleteTask} completeTask ={completeTask} completed = {task.completed}></Task>})}
+    </div>
+  
+  </div>)
 }
+
 
 export default App;
